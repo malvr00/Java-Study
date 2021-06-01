0036d6013572dafb8da120630e821438bf095ce1
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-06-02 00:36:52 +0900

    Update Io1.java

--- Io1/src/io1/Io1.java
+++ Io1/src/io1/Io1.java
@@ -6,12 +6,12 @@ public class Io1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		InputStream is = System.in;
-		OutputStream os = System.out;
+		InputStream is = System.in;		// Byte단위 Stream System.in(Keybord)
+		OutputStream os = System.out;	// Byte단위 Stream System.out(Monitor)
 		int inData;
 		try {
-			while((inData = is.read())!= -1) {
-				os.write(inData);
+			while((inData = is.read())!= -1) {	// Byte 단위 처리라 한글 안됌.
+				os.write(inData);	// 입출력의 기본형은 int char로 Casting해서 사용.
 				os.write('\n');
 			}
 			is.close();
