fb977271e244b877d131ae4ad4cd61a5f5d2dbaa
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-06-12 18:50:21 +0900

    Tetirs Game (Main)

---
+++ TetrisDoubleGame/src/tetrisDoubleGame/TetrisDoubleGame.java
@@ -0,0 +1,18 @@
+package tetrisDoubleGame;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+public class TetrisDoubleGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TetrisFrame mainFrame = new TetrisFrame("Tetris 2 ÀÎ¿ë");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.getContentPane().setBackground(Color.green);
+		mainFrame.setSize(600, 500);
+		mainFrame.setVisible(true);
+	}
+
+}
